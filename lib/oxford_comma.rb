eaa034d5c12c1a1c1323e0375b10576f173c3216
Kevin C Malin <racmalin@yahoo.com>
2020-04-26 15:18:02 +0000

    Done.

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,9 @@
 def oxford_comma(array)
-
-end
+  if array.size == 1 
+    array.join 
+    elsif array.size == 2 
+      array.join(" and ")
+    else 
+      "#{array[0..-2].join(", ")}, and #{array[-1]}"
+  end
+end 
