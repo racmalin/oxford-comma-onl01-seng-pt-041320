167704a93cd63eb8d760abe3f7bbefe96332ee00
Kevin C Malin <racmalin@yahoo.com>
2020-04-26 14:41:27 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
-end
+  if array.size == 1
+    array.join (" and ")
+    elsif array.size == 2
+    array.join (" and ")
+  elsif array.size >= 3
+    array[0..-2].join(" , ")  + " and "  << array[-1]
+    elsif array.join (" , ")
+  else array.join
+ end
+end 
